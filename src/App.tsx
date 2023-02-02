2c9e68d5720d81074642b12bf1e80890478e4315
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-02 17:59:46 +0100

    created fetch newsAPI logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const apiKey = "873bb42d84c34365a80ba866331d415f";
+  useEffect(() => {
+    getNews();
+  },[])
+
+  const getNews = async() => {
+    const news = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`);
+}
+
   return (
     <div className="App">
       <header className="App-header">
