52d577f69cbdb234c4dcf190ad502a9507ae2284
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-03 23:43:45 +0100

    added news content, published date and source to show on grid

--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -5,20 +5,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCamera } from "@fortawesome/free-solid-svg-icons"
 
 const NewsGrid: React.FC<{ newsItems: NewsType[] }> = (props) => {
+  console.log("test", props.newsItems)
   return (
     <div className="card-wrapper">
       {props.newsItems.map((news) => (
         <Card key={news.id}>
           {news.imageUrl && <img src={news.imageUrl} alt="news image" />}
           {!news.imageUrl && (
-            <div>
+            <div className="no-photo-wrapper">
               <div className="font-awesome-camera-wrapper">
                 <FontAwesomeIcon icon={faCamera} />
               </div>
               <p className="no-photo">No Photo</p>
             </div>
           )}
-          <p className="news-title"> {news.title}</p>
+          <div className="news-text-wrapper">
+            <p className="news-source">{news.source}</p>
+            <p className="news-title"> {news.title}</p>
+            <p className="news-content">{news.content}</p>
+            <p className="news-publised-date">{news.publishedDate}</p>
+          </div>
         </Card>
       ))}
     </div>
