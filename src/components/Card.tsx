bc16b72468e21299c2645a031fdfe9e179aeb252
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-03 18:34:25 +0100

    created card wrapper

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+import "./Card.scss"
+
+type Props = {
+  children?: React.ReactNode
+}
+const Card: React.FC<Props> = ({ children }) => {
+  return <div className="card">{children}</div>
+}
+
+export default Card
