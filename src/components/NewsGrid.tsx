364b0bf21bc9e3b6a1ab0709b0cd2594f9b2e009
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-03 18:34:01 +0100

    created grid to show news

--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -1,7 +1,18 @@
-const NewsGrid= ()=>{
+import Card from "./Card"
+import { NewsType } from "../types/globalTypes"
+
+const NewsGrid: React.FC<{ newsItems: NewsType[] }> = (props) => {
   return (
-    <div>test</div>
-  );
+    <div className="card-wrapper">
+      {props.newsItems.map((news) => (
+        <Card key={news.id}>
+          {news.imageUrl && <img src={news.imageUrl} alt="news image" />}
+          {!news.imageUrl && <p>No Photo</p>}
+          <h1> {news.title}</h1>
+        </Card>
+      ))}
+    </div>
+  )
 }
 
-export default NewsGrid;
+export default NewsGrid
