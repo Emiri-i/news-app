d6a9d2ff34c76ba5fa10d529c6a4eb0b1ad5c2b6
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-03 22:07:41 +0100

    created style when there is no photo image

--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -1,13 +1,23 @@
 import Card from "./Card"
 import { NewsType } from "../types/globalTypes"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCamera } from "@fortawesome/free-solid-svg-icons"
+
 const NewsGrid: React.FC<{ newsItems: NewsType[] }> = (props) => {
   return (
     <div className="card-wrapper">
       {props.newsItems.map((news) => (
         <Card key={news.id}>
           {news.imageUrl && <img src={news.imageUrl} alt="news image" />}
-          {!news.imageUrl && <p>No Photo</p>}
+          {!news.imageUrl && (
+            <div>
+              <div className="font-awesome-camera-wrapper">
+                <FontAwesomeIcon icon={faCamera} />
+              </div>
+              <p className="no-photo">No Photo</p>
+            </div>
+          )}
           <h1> {news.title}</h1>
         </Card>
       ))}
