d757d3e583a93b6156e2334f7608844f278e1dc7
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-05 17:21:01 +0100

    added error handling for fetch

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,36 @@ function App() {
   }, [])
 
   const getNews = async () => {
-    const data = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${APIKEY}`
-    )
-    const newsData = await data.json()
-    let newDataArray: NewsType[] = []
-    newsData.articles.forEach((news: returnedNewsDataType) => {
-      newDataArray.push({
-        id:
-          (news.title ? news.title : "") +
-          (news.publishedAt ? news.publishedAt : new Date().toISOString()),
-        title: news.title ? news.title : "",
-        description: news.description ? news.description : "",
-        url: news.url ? news.url : "",
-        imageUrl: news.urlToImage ? news.urlToImage : "",
-        content: news.content ? news.content : "",
-        publishedDate: news.publishedAt
-          ? new Date(news.publishedAt).toLocaleString()
-          : "",
-        source: news.source.name ? news.source.name : "",
-        author: news.author ? news.author : "",
+    try {
+      const data = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${APIKEY}`
+      )
+      const newsData = await data.json()
+      if (newsData.status !== "ok") {
+        throw new Error("Getting news data failed.")
+      }
+      let newDataArray: NewsType[] = []
+      newsData.articles.forEach((news: returnedNewsDataType) => {
+        newDataArray.push({
+          id:
+            (news.title ? news.title : "") +
+            (news.publishedAt ? news.publishedAt : new Date().toISOString()),
+          title: news.title ? news.title : "",
+          description: news.description ? news.description : "",
+          url: news.url ? news.url : "",
+          imageUrl: news.urlToImage ? news.urlToImage : "",
+          content: news.content ? news.content : "",
+          publishedDate: news.publishedAt
+            ? new Date(news.publishedAt).toLocaleString()
+            : "",
+          source: news.source.name ? news.source.name : "",
+          author: news.author ? news.author : "",
+        })
       })
-    })
-    setNewsItems(newDataArray)
+      setNewsItems(newDataArray)
+    } catch (e) {
+      window.alert(e)
+    }
   }
 
   return (
