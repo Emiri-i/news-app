6eed277b5d7267df15212f79fe234ef7ba528900
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-05 17:02:48 +0100

    added link to go to the original web source page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,19 @@
 import "./Card.scss"
+import { NewsType } from "../types/globalTypes"
 
 type Props = {
+  newsItem: NewsType
   children?: React.ReactNode
 }
-const Card: React.FC<Props> = ({ children }) => {
-  return <div className="card">{children}</div>
+const Card: React.FC<Props> = ({ newsItem, children }) => {
+  const test = () => {
+    window.open(newsItem.url)
+  }
+  return (
+    <div className="card" onClick={test}>
+      {children}
+    </div>
+  )
 }
 
 export default Card
