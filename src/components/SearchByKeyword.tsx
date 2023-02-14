7942a0a58a8dbc44ba98e5c14703ce8fc82e730e
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-14 18:23:51 +0100

    modified css in header category and keyword section

--- src/components/SearchByKeyword.tsx
+++ src/components/SearchByKeyword.tsx
@@ -2,16 +2,19 @@ import "./SearchByKeyword.scss"
 
 const SearchByKeyWord = () => {
   return (
-    <>
+    <div className="search-keyword-wrapper">
       <label>
         <div className="search-keyword-label-wrapper">
           <div className="search-keyword-label">Search By Keyword</div>
         </div>
-        <div className="search-keyword-input-wrapper">
-          <input className="search-keyword-input" type="text" />
+        <div className="search-input-section-wrapper">
+          <div className="search-keyword-input-wrapper">
+            <input className="search-keyword-input" type="text" />
+          </div>
+          <button className="search-button">search</button>
         </div>
       </label>
-    </>
+    </div>
   )
 }
 
