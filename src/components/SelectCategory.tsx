c13a5fa464a10db8cfd26dcc633c88db0f27b259
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-04-20 17:46:17 +0200

    refactoring

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -1,57 +1,52 @@
-import React, { useEffect, useContext } from "react"
-import { newsItemsContext } from "../store/newsItemContext"
+import React, { useEffect, useContext } from "react";
+import { newsItemsContext } from "../store/newsItemContext";
 
-import "./SelectCategory.scss"
+import "./SelectCategory.scss";
 
 const SelectCategory: React.FC = () => {
-  const newsCtx = useContext(newsItemsContext)
+  const newsCtx = useContext(newsItemsContext);
+  const categoryList = [
+    "Business",
+    "Entertainment",
+    "Health",
+    "Science",
+    "Sports",
+    "Technology",
+  ];
   useEffect(() => {
-    clickCategory()
-  }, [])
+    clickCategory();
+  }, []);
   useEffect(() => {
-    changeCategory()
-  }, [newsCtx.newsCategoryName])
+    changeCategory();
+  }, [newsCtx.newsCategoryName]);
   const clickCategory = (e?: React.MouseEvent) => {
     if (e) {
-      const eventTarget = e.target as HTMLElement
-      newsCtx.setNewsCategoryName(eventTarget.innerHTML.toLowerCase())
+      const eventTarget = e.target as HTMLElement;
+      newsCtx.setNewsCategoryName(eventTarget.innerHTML.toLowerCase());
     }
-  }
+  };
 
   const changeCategory = () => {
     const categoryElement =
-      document.querySelectorAll<HTMLInputElement>(".category")
+      document.querySelectorAll<HTMLInputElement>(".category");
     categoryElement.forEach((el: HTMLElement) => {
       if (newsCtx.newsCategoryName === el.innerHTML.toLowerCase()) {
-        el.className = "category current"
+        el.className = "category current";
       } else {
-        el.className = "category"
+        el.className = "category";
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className="category-wrapper">
-      <button onClick={clickCategory} className="category">
-        Business
-      </button>
-      <button onClick={clickCategory} className="category">
-        Entertainment
-      </button>
-      <button onClick={clickCategory} className="category">
-        Health
-      </button>
-      <button onClick={clickCategory} className="category">
-        Science
-      </button>
-      <button onClick={clickCategory} className="category">
-        Sports
-      </button>
-      <button onClick={clickCategory} className="category">
-        Technology
-      </button>
+      {categoryList.map((list) => (
+        <button onClick={clickCategory} className="category">
+          {list}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SelectCategory
+export default SelectCategory;
